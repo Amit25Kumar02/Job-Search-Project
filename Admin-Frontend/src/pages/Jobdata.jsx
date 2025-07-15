bb49047d84239e28d57d9ca48bab58d3e9319897
AMIT DHATTERWAL <amitchitain@gmail.com>
2025-07-15 12:02:31 +0530

    updating admin panel for featching job

--- Admin-Frontend/src/pages/Jobdata.jsx
+++ Admin-Frontend/src/pages/Jobdata.jsx
@@ -22,7 +22,7 @@ const JobData = () => {
     }
 
     try {
-      const response = await axios.get(`${API_URL}/api/jobs/all`, {
+      const response = await axios.get(`${API_URL}/api/Ajobs/all`, {
         headers: {
           Authorization: `Bearer ${token}`, // Send the token in headers
         },
