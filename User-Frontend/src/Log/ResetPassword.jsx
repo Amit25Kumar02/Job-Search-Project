b1e228324e6fe56a4cf02d673cefd4e3820c8d3a
AMIT DHATTERWAL <amitchitain@gmail.com>
2025-08-06 10:13:11 +0530

    api link change in ResetPassword page

--- User-Frontend/src/Log/ResetPassword.jsx
+++ User-Frontend/src/Log/ResetPassword.jsx
@@ -19,7 +19,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const res = await axios.post(`${API_URL}/api/auth/reset-password`, {
+      const res = await axios.post(`${API_URL}/api/users/reset-password`, {
         token,
         newPassword: password,
       });
